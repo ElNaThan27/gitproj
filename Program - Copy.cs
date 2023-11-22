3066221afa218706626da6c1b48c8911d9d65bb4
Gabriel Nathaniel <gabnath.h27@gmail.com>
2023-11-22 19:01:44 +0700

    last ni ke 10

---
+++ Program - Copy.cs
@@ -0,0 +1,15 @@
+int a = 20;
+int b = 5;
+int c = a+b;
+int d = a-b;
+int e = a*b;
+int f = a/b;
+int g = a%b;
+
+Console.WriteLine($"Bil1= {a}");
+Console.WriteLine($"Bil2= {b}");
+Console.WriteLine("Hasil Penjumlahan dari Bil1 + Bil2= "+c);
+Console.WriteLine("Hasil Pengurangan dari Bil1 - Bil2= "+d);
+Console.WriteLine("Hasil Perkalian dari Bil1 x Bil2= "+e);
+Console.WriteLine("Hasil Pembagian dari Bil1 : Bil2= "+f);
+Console.WriteLine("Hasil Modulus dari Bil1 % Bil2= "+g);
